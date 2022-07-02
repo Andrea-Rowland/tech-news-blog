60f683fafd6d5f55a21b4f156c8e3bd3b48616e5
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-07-01 19:49:21 -0600

    testing routes on insomnia

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 
-router.user('/users', userRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const path = require('path');
 const express = require('express');
+const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const helpers = require('./utils/helpers');
+const { sequelize } = require('./models/user');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
 
