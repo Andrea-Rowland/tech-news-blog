190028cda76de4ea6eec46cee4d2f6bfab71ee55
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-07-01 18:59:45 -0600

    completed user.js

--- models/user.js
+++ models/user.js
@@ -6,3 +6,61 @@
 // THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post
 // WHEN I click on one of my existing posts in the dashboard
 // THEN I am able to delete or update my post and taken back to an updated dashboard
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        //define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        //define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false.valueOf,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            },
+            async beforeUpdate(newUserData) {
+                newUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            }
+        },
+            
+        // pass in our imported sequelize connection ( the direct connection to our database)
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
