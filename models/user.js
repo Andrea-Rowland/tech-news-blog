8069f3ddc37a5df62b9f7ec0291772231693b47e
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-07-01 20:28:36 -0600

    debugged user-routes with Insomnia

--- models/user.js
+++ models/user.js
@@ -1,12 +1,3 @@
-// WHEN I click on the dashboard option in the navigation
-// THEN I am taken to the dashboard and presented with any blog posts I have already created and the option to add a new blog post
-// WHEN I click on the button to add a new blog post
-// THEN I am prompted to enter both a title and contents for my blog post
-// WHEN I click on the button to create a new blog post
-// THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post
-// WHEN I click on one of my existing posts in the dashboard
-// THEN I am able to delete or update my post and taken back to an updated dashboard
-
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -45,15 +36,7 @@ User.init(
         }
     },
     {
-        hooks: {
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-            },
-            async beforeUpdate(newUserData) {
-                newUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-            }
-        },
-            
+               
         // pass in our imported sequelize connection ( the direct connection to our database)
         sequelize,
         timestamps: false,
