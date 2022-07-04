b461bea53e53b06874ecb3e5c1c94b815497a86b
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-07-04 10:34:25 -0600

    debugging create error

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-router.get('/', (req, res) => {
-    
-});
+// router.get('/', (req, res) => {
+//     Comment.findAll({
+//         attributes: ['id', 'comment_text', 'user_id', 'post_id'],
+//         include: [
+//             {
+//                 model: User,
+//                 attributes: ['user_id']
+//             },
+//             {
+//                 model: Post,
+//                 attributes: ['post_id']
+//             }
+//         ]
+//     })
+//     .then(dbCommentData => res.json(dbCommentData))
+//     .catch(err => {
+//         console.log(err);
+//         res.status(400).json(err);
+//     });
+// });
 
 router.post('/', (req, res) => {
     console.log('I am here.');
